e0304d23b5ce010e082d74dc0a0d48e45b7a73da
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-08 15:25:48 +0100

    IA-2750: add govuk-user-feedback-dev (skeleton only)

---
+++ gcp-govuk-user-feedback-dev/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "govuk"
+    workspaces {
+      project = "govuk-data-engineering"
+      name    = "gcp-govuk-user-feedback-dev"
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.33.0"
+    }
+  }
+
+  required_version = "~> 1.15"
+}
+
+provider "google" {
+  project = "govuk-user-feedback-dev"
+}

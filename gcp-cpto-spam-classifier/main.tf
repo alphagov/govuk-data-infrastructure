c2dbc2ea87cfcda02c3b0aa3f6d0802503cd8312
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-11 09:43:19 +0100

    IA-2750: add cpto-spam-classifier

--- gcp-cpto-spam-classifier/main.tf
+++ gcp-cpto-spam-classifier/main.tf
@@ -20,3 +20,22 @@ terraform {
 provider "google" {
   project = "cpto-spam-classifier"
 }
+
+module "managed_project" {
+  source = "../modules/gcp-project-init"
+
+  project_id   = "cpto-spam-classifier"
+  project_name = "cpto-spam-classifier"
+
+  project_owners = ["group:gcp-data-infrastructure-owners@digital.cabinet-office.gov.uk"]
+  project_editors = [
+    "group:gcp-data-infrastructure-editors@digital.cabinet-office.gov.uk",
+    "serviceAccount:cpto-spam-classifier-sa@cpto-spam-classifier.iam.gserviceaccount.com",
+    "serviceAccount:cpto-spam-classifier@appspot.gserviceaccount.com",
+
+  ]
+  project_viewers = [
+    "group:gcp-data-infrastructure-viewers@digital.cabinet-office.gov.uk",
+
+  ]
+}

dbc17c9d5c2acc8cf9ad8eea2ac1ff7bede2acc3
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-11 09:22:08 +0100

    IA-2750: add gds-gcp-admin-di

---
+++ gcp-gds-gcp-admin-di/imports.tf
@@ -0,0 +1,4 @@
+import {
+  id = "gds-gcp-admin-di"
+  to = module.managed_project.google_project.project
+}
--- gcp-gds-gcp-admin-di/main.tf
+++ gcp-gds-gcp-admin-di/main.tf
@@ -20,3 +20,19 @@ terraform {
 provider "google" {
   project = "gds-gcp-admin-di"
 }
+
+module "managed_project" {
+  source = "../modules/gcp-project-init"
+
+  project_id   = "gds-gcp-admin-di"
+  project_name = "GDS-GCP-Admin-DI"
+
+  billing_account = "01F74E-53D4C7-16BF6B"
+
+  project_owners = ["group:gcp-data-infrastructure-owners@digital.cabinet-office.gov.uk"]
+  project_editors = [
+    "group:gcp-data-infrastructure-editors@digital.cabinet-office.gov.uk",
+    "serviceAccount:6322611068-compute@developer.gserviceaccount.com",
+  ]
+  project_viewers = ["group:gcp-data-infrastructure-viewers@digital.cabinet-office.gov.uk"]
+}

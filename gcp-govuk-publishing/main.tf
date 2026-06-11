6419b9b64fa7790c901b59f25e93cc987ddab4c8
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-11 14:21:04 +0100

    add owners group for govuk-publishing

--- gcp-govuk-publishing/main.tf
+++ gcp-govuk-publishing/main.tf
@@ -26,12 +26,11 @@ module "managed_project" {
 
   project_id     = "govuk-publishing"
   project_name   = "govuk-publishing"
-  project_owners = ["group:gcp-data-infrastructure-owners@digital.cabinet-office.gov.uk"]
+  project_owners = ["group:gcp-govuk-publishing-owners@digital.cabinet-office.gov.uk"]
   project_editors = [
-    "group:gcp-data-infrastructure-editors@digital.cabinet-office.gov.uk",
     "serviceAccount:govuk-publishing@appspot.gserviceaccount.com",
     "serviceAccount:analytics-processing-dev@system.gserviceaccount.com",
     "serviceAccount:942729121218-compute@developer.gserviceaccount.com",
   ]
-  project_viewers = ["group:gcp-data-infrastructure-viewers@digital.cabinet-office.gov.uk"]
+  project_viewers = []
 }

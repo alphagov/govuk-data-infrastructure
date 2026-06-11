95376fd13cb7f9d5cc0ae653bfae592b864eea21
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-11 09:01:35 +0100

    IA-2750: add sde-prototype-service

---
+++ gcp-sde-prototype-service/imports.tf
@@ -0,0 +1,4 @@
+import {
+  id = "sde-prototype-service"
+  to = module.managed_project.google_project.project
+}
--- gcp-sde-prototype-service/main.tf
+++ gcp-sde-prototype-service/main.tf
@@ -20,3 +20,19 @@ terraform {
 provider "google" {
   project = "sde-prototype-service"
 }
+
+module "managed_project" {
+  source = "../modules/gcp-project-init"
+
+  project_id   = "sde-prototype-service"
+  project_name = "sde-prototype-service"
+
+  project_owners = ["group:gcp-data-infrastructure-owners@digital.cabinet-office.gov.uk"]
+  project_editors = [
+    "group:gcp-data-infrastructure-editors@digital.cabinet-office.gov.uk",
+    "serviceAccount:503350198460-compute@developer.gserviceaccount.com",
+    "serviceAccount:firebase-measurement@system.gserviceaccount.com",
+    "serviceAccount:sde-prototype-service@appspot.gserviceaccount.com",
+  ]
+  project_viewers = ["group:gcp-data-infrastructure-viewers@digital.cabinet-office.gov.uk"]
+}

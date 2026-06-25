a022ff53e7d7e0de83a960c3fd9062d625c7ec89
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-06-25 10:26:42 +0100

    IA-2955: make aggregate-analytics SA jobUser in gds-bq-processing

--- gcp-gds-bq-processing/service_account_iam.tf
+++ gcp-gds-bq-processing/service_account_iam.tf
@@ -12,9 +12,10 @@ resource "google_service_account_iam_member" "dataform_agent_impersonation" {
   member             = "serviceAccount:service-${google_project.project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
 }
 
-resource "google_service_account_iam_member" "aggregate_analytics_pipeline_executor_impersonation_binding" {
+resource "google_service_account_iam_member" "dataform_aggregate_analytics_agent_impersonation" {
+  for_each           = toset(local.dataform_sa_impersonation_roles)
   service_account_id = google_service_account.aggregate_analytics_pipeline_executor.name
-  role               = "roles/iam.serviceAccountTokenCreator"
+  role               = each.key
   member             = "serviceAccount:service-${google_project.project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
 }
 
@@ -22,4 +23,10 @@ resource "google_service_account_iam_member" "aggregate_analytics_pipeline_execu
   service_account_id = google_service_account.aggregate_analytics_pipeline_executor.name
   role               = "roles/iam.serviceAccountUser"
   member             = "group:gcp-gds-bq-processing-editors@digital.cabinet-office.gov.uk"
+}
+
+resource "google_project_iam_member" "bq_job_user" {
+  project = google_project.project.project_id
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.aggregate_analytics_pipeline_executor.email}"
 }

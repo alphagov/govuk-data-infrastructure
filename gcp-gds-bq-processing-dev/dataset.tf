d0b4899a345fcad6a611a1f6e83f21e39854eef8
Alex Faria <alex.faria@dsit.gov.uk>
2026-08-18 10:41:36 +0100

    Add fastly processing datasets

--- gcp-gds-bq-processing-dev/dataset.tf
+++ gcp-gds-bq-processing-dev/dataset.tf
@@ -22,3 +22,18 @@ resource "google_bigquery_dataset" "processing_dev" {
   dataset_id = "processing_dev"
   location   = "europe-west2"
 }
+
+resource "google_bigquery_dataset" "fastly_processing_dev" {
+  dataset_id = "fastly_processing_dev"
+  location   = "europe-west2"
+}
+
+resource "google_bigquery_dataset" "fastly_target_dev" {
+  dataset_id = "fastly_target_dev"
+  location   = "europe-west2"
+}
+
+resource "google_bigquery_dataset" "fastly_dataform_assertions" {
+  dataset_id = "fastly_dataform_assertions"
+  location   = "europe-west2"
+}

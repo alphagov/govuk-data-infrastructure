b867c11c770b5e6f458f1288d8f7c84ee1375720
aaronfowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-08-27 09:36:02 +0100

    import fastly dataform datasets as they already exist

--- gcp-gds-bq-processing-dev/imports.tf
+++ gcp-gds-bq-processing-dev/imports.tf
@@ -1,24 +1,19 @@
 import {
-  to = google_bigquery_dataset.analytics_330577055_dev
-  id = "projects/gds-bq-processing-dev/datasets/analytics_330577055_dev"
+  to = google_bigquery_dataset.fastly_processing_dev
+  id = "projects/gds-bq-processing-dev/datasets/fastly_processing_dev"
 }
 
 import {
-  to = google_bigquery_dataset.dataform_assertions
-  id = "projects/gds-bq-processing-dev/datasets/dataform_assertions"
+  to = google_bigquery_dataset.fastly_target_dev
+  id = "projects/gds-bq-processing-dev/datasets/fastly_target_dev"
 }
 
 import {
-  to = google_bigquery_dataset.flattened_dataset_dev
-  id = "projects/gds-bq-processing-dev/datasets/flattened_dataset_dev"
+  to = google_bigquery_dataset.fastly_dataform_assertions
+  id = "projects/gds-bq-processing-dev/datasets/fastly_dataform_assertions"
 }
 
 import {
-  to = google_bigquery_dataset.govuk_ga4_processing_dev
-  id = "projects/gds-bq-processing-dev/datasets/govuk_ga4_processing_dev"
-}
-
-import {
-  to = google_bigquery_dataset.processing_dev
-  id = "projects/gds-bq-processing-dev/datasets/processing_dev"
+  to = google_bigquery_dataset.fastly_reference_data_dev
+  id = "projects/gds-bq-processing-dev/datasets/fastly_reference_data_dev"
 }

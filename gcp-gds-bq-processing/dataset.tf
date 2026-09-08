82e8393934e7fe11592abe446c639b9ba472d098
Alex Faria <alex.faria@dsit.gov.uk>
2026-09-08 11:56:34 +0100

    add fastly datasets to gds-bq-processing prod added to dataset.tf also

--- gcp-gds-bq-processing/dataset.tf
+++ gcp-gds-bq-processing/dataset.tf
@@ -9,3 +9,15 @@ resource "google_bigquery_dataset" "govuk_ga4_processing" {
   project    = google_project.project.project_id
   location   = "europe-west2"
 }
+
+resource "google_bigquery_dataset" "fastly_reference_data" {
+  dataset_id = "fastly_reference_data"
+  project    = google_project.project.project_id
+  location   = "europe-west2"
+}
+
+resource "google_bigquery_dataset" "fastly_dataform_assertions" {
+  dataset_id = "fastly_dataform_assertions"
+  project    = google_project.project.project_id
+  location   = "europe-west2"
+}

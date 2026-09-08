ba2a5432df8dcce0634b19072bf09ddbb3256975
Alex Faria <alex.faria@dsit.gov.uk>
2026-09-08 11:00:59 +0100

    add fastly datasets to gds-bq-processing prod

---
+++ gcp-gds-bq-processing/imports.tf
@@ -0,0 +1,9 @@
+import {
+  to = google_bigquery_dataset.fastly_dataform_assertions
+  id = "projects/gds-bq-processing/datasets/fastly_dataform_assertions"
+}
+
+import {
+  to = google_bigquery_dataset.fastly_reference_data
+  id = "projects/gds-bq-processing/datasets/fastly_reference_data"
+}

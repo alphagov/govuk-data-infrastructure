7f2a200b4fdbff6541cb4137fbff33ed0e198490
aaronfowles <aaron.fowles@digital.cabinet-office.gov.uk>
2026-09-15 10:16:09 +0100

    IA-3150: add exclusions to allow_list for gds-bq-reporting scheduled queries

--- gcp-gds-bq-reporting/main.tf
+++ gcp-gds-bq-reporting/main.tf
@@ -39,4 +39,9 @@ module "scheduled_query_monitoring" {
   notification_email_address      = data.google_secret_manager_secret_version.slack_alert_channel_email_address.secret_data
   documentation_link_display_name = "Data Community Technical Documentation"
   documentation_link_url          = "https://docs.data-community.publishing.service.gov.uk/tools/google-cloud-platform/gcp-projects/#gds-bigquery-reporting"
+  allow_list = [
+    "66869a14-0000-2576-92ed-c82add77914c", # Content data checker
+    "67234dfd-0000-2f85-9ef3-14c14ef19c68", # partition_flat_checker
+    "66616cd0-0000-2ed7-b95a-14223bba1176", # raw_data_table_checker
+  ]
 }
